ddce50db931714c1f19ca1bf008c4b84309ac6a3
Gustav Kutugutov <gustav.kutugutov@gmail.com>
2026-03-17 08:50:48 +0500

    iter23 build info

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -25,7 +25,27 @@ import (
 	"github.com/Gustik/shortener/internal/zaplog"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
+func printBuildInfo() {
+	na := func(s string) string {
+		if s == "" {
+			return "N/A"
+		}
+		return s
+	}
+	fmt.Printf("Build version: %s\n", na(buildVersion))
+	fmt.Printf("Build date: %s\n", na(buildDate))
+	fmt.Printf("Build commit: %s\n", na(buildCommit))
+}
+
 func main() {
+	printBuildInfo()
+
 	cfg := config.Load()
 
 	logger, err := zaplog.New(cfg.LogLevel)
